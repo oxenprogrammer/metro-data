89ea0e72fc4e4f77f3b3a33ef6a5c111f50a3803
oxenprogrammer <emmy1000okello@gmail.com>
2019-05-13 14:19:02 +0300

    ADD: write the service module

---
+++ src/app/app.service.ts
@@ -0,0 +1,26 @@
+import { environment } from './../environments/environment';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+
+
+@Injectable()
+export class ServiceNameService {
+
+  url = environment.serverURL;
+
+  constructor(private http: HttpClient) { }
+
+  getData(): Observable<any> {
+    return this.http.get<any>(this.url)
+      .pipe(
+        tap((data: any) => {
+          return of(data);
+        })
+      );
+  }
+
+}
